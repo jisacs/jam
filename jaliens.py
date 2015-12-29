aed642f2bb298ca388a2a8ab2a9f38f8a5b6c3d8
Jean Jacquemier <jacquem@lapp.in2p3.fr>
2015-12-29 23:32:01 +0100

    on place la route

--- jaliens.py
+++ jaliens.py
@@ -17,7 +17,7 @@ MAX_SHOTS      = 2      #most player bullets onscreen
 ALIEN_ODDS     = 22     #chances a new alien appears
 BOMB_ODDS      = 60    #chances a new bomb will drop
 ALIEN_RELOAD   = 12     #frames between new aliens
-SCREENRECT     = Rect(0, 0, 640, 480)
+SCREENRECT     = Rect(0, 0, 6400, 4800)
 
 IMAGE_UP = 0
 IMAGE_DOWN = 1
